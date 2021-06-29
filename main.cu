9d10cfa4f69c2a29bb3949ae2c888dd52e214288
Danny Fuentes <danny@firstjob.cl>
2021-06-29 18:14:13 -0400

    WIP: pregunta f

--- main.cu
+++ main.cu
@@ -4,7 +4,6 @@
 #include <stdlib.h>
 #include <math.h>
 #define BS 256
-#define T 16
 /*
  *  Generador de matriz
  */
@@ -12,8 +11,8 @@ void genMatrix(int** matrix, int N, int M) {
     int* matrix1 = new int[M*N];
     srand(1);
 	for(int i = 0; i < M*N; i++)
-		//matrix1[i] = rand() % 1000 +1;
-        matrix1[i] = 1;
+		matrix1[i] = rand() % 1000 +1;
+        // matrix1[i] = 1;
     *matrix = matrix1;
 }
 
@@ -80,7 +79,8 @@ __global__ void kernelb(int *A, int *x, int *b, int N){
 __global__ void kernelRed(int *A, int *x, int *b, int N){
     int tid = threadIdx.x + blockIdx.x * blockDim.x;
     if (tid < N){
-        __shared__ int ax[BS];
+        extern __shared__ int ax[];
+        int size;
         for(int i = 0; i < N; i++){
             ax[threadIdx.x] = A[i*N + tid]*x[tid];
             __syncthreads();
@@ -89,7 +89,7 @@ __global__ void kernelRed(int *A, int *x, int *b, int N){
             //         atomicAdd(ax[threadIdx.x % (BS/(2**j))],ax[threadIdx.x]);
             //     }
             // }
-            for (int size = BS/2; size>0; size/=2) {
+            for (size = 256/2; size>0; size/=2) {
                 if (threadIdx.x<size) atomicAdd(&ax[threadIdx.x], ax[threadIdx.x+size]);
                 __syncthreads();
             }
@@ -100,42 +100,48 @@ __global__ void kernelRed(int *A, int *x, int *b, int N){
     }
 }
 
+// bs = 2
+// 1 2 3 4         10
+// 4 2 3 4         x2
+// 2 2 3 4         x3
+// 6 2 3 4         x4
+
+
 
 /*
   *  Kernel inciso E
   */
-// __global__ void kernelSM(int *A, int *x, int *b, int N){
-//     int tid = threadIdx.x + blockIdx.x * blockDim.x;
-//     __shared__ int vx[BS];
-// 	if (tid < N){
-//         vx[threadIdx.x] = x[tid];
-//         __syncthreads();
-//         int total = 0;
-//         for(int i = 0; i < BS; i++){
-//             atomicAdd(total, A[i+tid*BS]*vx[]);
-//             // atomicAdd(total, A[tid+i*N]*vx[tid]);
-//             // total += A[i]*vx[i];
-//         }
-// 	}
-// }
-
-
-int * suma(int * A, int * X, int N){
-    int s;
-    int * b = new int[N];
-    int k = 0;
-    for(int i = 0; i < N*N; i=i+N){
-        s = 0; 
-        for(int j = i; j < i+N; j++){
-            s += A[j] * X[j%N];
+__global__ void kernelSM(int *A,int *x, int *b, int N){
+    //calcular por columna de A y por filas de x
+    //guardar el valor en variable local y al final hacer un atomicAdd
+    int tid = threadIdx.x + blockIdx.x * blockDim.x;
+    extern __shared__ int vx[];
+	if (tid < N){
+        int total = 0;
+        for(int i = 0; i < N; i++){
+            vx[threadIdx.x] = x[i];
+            __syncthreads();
+            total += A[tid*N + i]*vx[threadIdx.x];
+            // __syncthreads();
         }
-        b[k] = s;
-        k++;
-    }
-    return b;
+        atomicAdd(&b[tid], total);
+	}
 }
 
-
+/*
+  *  Kernel inciso F
+  */
+__constant__ int x[10000];
+__global__ void kernelCM(int *A, int *b, int N){
+    int tid = threadIdx.x + blockIdx.x * blockDim.x;
+    if (tid < N){
+        int total = 0;
+        for(int i = 0; i < N; i++){
+            total += A[i+tid*N]*x[i];
+        }
+        b[tid] = total;
+    }
+}
 
 
 int main(){
@@ -151,20 +157,7 @@ int main(){
 
     genMatrix(&Ahost, N, M);
 	genMatrix(&xhost, 1, N);
-    int * a = suma(Ahost, xhost, N);
-/*
-    for(int i = 0; i < N*M; i++){
-        printf("%d ", Ahost[i]);
-    }
-    printf("\n"); 
-    for(int i = 0; i < N; i++){
-        printf("%d ", xhost[i]);
-    }
-    printf("\n"); 
-    for(int i = 0; i < N; i++){
-        printf("%d ", a[i]);    
-    }
-*/
+    
     //inciso a
 
     cudaMalloc((void**)&A, N * M * sizeof(int));
@@ -231,11 +224,11 @@ int main(){
     cudaEventRecord(ct2);
     cudaEventSynchronize(ct2);
     cudaEventElapsedTime(&dt, ct1, ct2);
-	//printf("c) Tiempo GPU: %f[ms]\n", dt);
+	printf("c) Tiempo GPU: %f[ms]\n", dt);
 
 	bhost = new int[M];
 	cudaMemcpy(bhost, b, M * sizeof(int), cudaMemcpyDeviceToHost);
-    //printf("%d\n", bhost[1]);
+    // printf("%d\n", bhost[1]);
     for(int i = 0; i < 10; i++){
        printf("%d ", bhost[i]);    
     }
@@ -256,11 +249,11 @@ int main(){
     cudaEventCreate(&ct2);
     cudaEventRecord(ct1);
     //printf("gs: %d  bs:%d\n", gs, bs);
-    kernelRed<<<gs, bs>>>(A, x, b, N);
+    kernelRed<<<gs, bs, bs*sizeof(int)>>>(A, x, b, N);
     cudaEventRecord(ct2);
     cudaEventSynchronize(ct2);
     cudaEventElapsedTime(&dt, ct1, ct2);
-	//printf("d) Tiempo GPU: %f[ms]\n", dt);
+	printf("d) Tiempo GPU: %f[ms]\n", dt);
 
 	bhost = new int[M]();
 	cudaMemcpy(bhost, b, M * sizeof(int), cudaMemcpyDeviceToHost);
@@ -268,6 +261,66 @@ int main(){
         printf("%d ", bhost[i]);    
     }
     printf("\n-----------------------------------------------------------------------\n");
+    // printf("%d\n", bhost[1]);
+	delete[] bhost;
+	cudaFree(b); cudaFree(A); cudaFree(x);
+
+
+    //inciso E
+    gs = (int)ceil((float)N / bs);
+    cudaMalloc((void**)&A, N * M * sizeof(int));
+    cudaMalloc((void**)&x, N * sizeof(int));
+    cudaMemcpy(A, Ahost, N * M * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(x, xhost, N * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMalloc((void**)&b, M * sizeof(int));
+	cudaMemset(b, 0, M);
+
+    cudaEventCreate(&ct1);
+    cudaEventCreate(&ct2);
+    cudaEventRecord(ct1);
+    //printf("gs: %d  bs:%d\n", gs, bs);
+    kernelSM<<<gs, bs, bs*sizeof(int)>>>(A, x, b, N);
+    cudaEventRecord(ct2);
+    cudaEventSynchronize(ct2);
+    cudaEventElapsedTime(&dt, ct1, ct2);
+	printf("e) Tiempo GPU: %f[ms]\n", dt);
+
+	bhost = new int[M]();
+	cudaMemcpy(bhost, b, M * sizeof(int), cudaMemcpyDeviceToHost);
+    for(int i = 0; i < 10; i++){
+        printf("%d ", bhost[i]);    
+    }
+     printf("\n-----------------------------------------------------------------------\n");
+    // printf("%d\n", bhost[1]);
+	delete[] bhost;
+	cudaFree(b); cudaFree(A); cudaFree(x);
+
+    //inciso F
+    gs = (int)ceil((float)N / bs);
+    cudaMalloc((void**)&A, N * M * sizeof(int));
+    //cudaMalloc((void**)&x, N * sizeof(int));
+    cudaMemcpy(A, Ahost, N * M * sizeof(int), cudaMemcpyHostToDevice);
+    //cudaMemcpy(x, xhost, N * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMalloc((void**)&b, M * sizeof(int));
+	cudaMemset(b, 0, M);
+    cudaMemcpyToSymbol(x, xhost, N*sizeof(int), 0, cudaMemcpyHostToDevice);
+
+    cudaEventCreate(&ct1);
+    cudaEventCreate(&ct2);
+    cudaEventRecord(ct1);
+    //printf("gs: %d  bs:%d\n", gs, bs);
+    kernelCM<<<gs, bs>>>(A, b, N);
+    cudaEventRecord(ct2);
+    cudaEventSynchronize(ct2);
+    cudaEventElapsedTime(&dt, ct1, ct2);
+	printf("f) Tiempo GPU: %f[ms]\n", dt);
+
+	bhost = new int[M]();
+	cudaMemcpy(bhost, b, M * sizeof(int), cudaMemcpyDeviceToHost);
+    for(int i = 0; i < 10; i++){
+        printf("%d ", bhost[i]);    
+    }
+    // printf("\n-----------------------------------------------------------------------\n");
     // printf("%d\n", bhost[1]);
 	delete[] bhost;
 	cudaFree(b); cudaFree(A); cudaFree(x);
